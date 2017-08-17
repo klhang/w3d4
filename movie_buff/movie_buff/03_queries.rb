45c1103969bbb3b6e75c27e54f94dd77bc7b520a
App Academy Student <student@appacademy.io>
2017-08-17 15:13:20 -0700

    movie_buff

--- movie_buff/movie_buff/03_queries.rb
+++ movie_buff/movie_buff/03_queries.rb
@@ -1,11 +1,23 @@
 def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
+  Movie
+    .select(:title, :id)
+    .joins(:actors)
+    .where('actors.name in (?)', those_actors)
+    .group(:id)
+    .having('COUNT(actors.id) >= ?', those_actors.length)
 
 end
 
 def golden_age
   # Find the decade with the highest average movie score.
+  Movie
+    .select('(yr-(yr%10)) as decade, AVG(score)')
+    .group('decade')
+    .order('AVG(score) desc')
+    .first
+    .decade
 
 end
 
@@ -13,6 +25,14 @@ def costars(name)
   # List the names of the actors that the named actor has ever
   # appeared with.
   # Hint: use a subquery
+  Actor
+  
+
+  sub = (dsd
+  Movie # all movies where the actor has appeared
+    .select(:title)
+    .join(:actors)
+    .where('actors.name IN (?)', name)
 
 end
 
